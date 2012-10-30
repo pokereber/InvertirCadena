5162107890d27968d5dfd53c1631eb68d373fe49
chable <robert_dpinio@hotmail.com>
2012-10-29 18:47:32 -0600

    agregamos nombre de escuela

--- invertircadena/src/invertircadena/Invertircadena.java
+++ invertircadena/src/invertircadena/Invertircadena.java
@@ -16,6 +16,6 @@ public class Invertircadena
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+     System.out.print("INSTITUTO TECNOLOGICO SUPERIOR DE ESCRACEGA");
     }
 }
